2eae39a48d0d4179bd825e6ec6ca1bf0a5524945
Preetham N Reddy <preethamnr1997@gmail.com>
2017-01-07 14:44:04 +0530

    Minor changes to add logging functionality

--- game.py
+++ game.py
@@ -10,10 +10,22 @@ class ScotlandYard(object):
         self.G = []
         self.detectives = []
         self.MRx = []
-        self.prev_5 = [] #Notes down the last 5 transactions by MRx
-        self.last_location = [] #The last location of MRx
         self.time_list = [0,0,0,0,0]
+        self.locations = [0] * 24
+        self.transport_log = [[0,0,0]] * 24
         self.starting_nodes = const.choose_starting_nodes()
+        self.turn_number = 0 # Retains the turn number
+        self.turn_sub_counter = 0 #Retains whose turn it is during the specific turn
+                                  #0 - MRx, [1->5] for detectives iteratively
+        self.complete = False
+        self.detective_moves = [True,True,True,True,True] #If any detective is in a location where he does
+                                                          #not have the token to move away from, it is set to False
+
+        #For metadata and debugging, we will need more data. The following variables will store metadata
+        self.detectives_transport_log = [self.transport_log] * 5
+        self.detectives_location_log = [self.locations] * 5
+        
+
 
 
     def initialize_game(self):
@@ -22,6 +34,3 @@ class ScotlandYard(object):
         for i in range(5):
             self.detectives.append(d_util.initialize_detective(self.starting_nodes[i+1]))
         self.MRx = x_util.initialize_x(self.starting_nodes[0])
-        self.prev_5 = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        self.last_location = 0
-        
--- utilities/const.py
+++ utilities/const.py
@@ -3,11 +3,13 @@ import numpy as np
 start_taxi = 10
 start_bus = 8
 start_underground = 4
-start_taxi_x = 1
-start_bus_x = 1
-start_underground_x = 1
+start_taxi_x = 4
+start_bus_x = 3
+start_underground_x = 3
 
 starting_nodes = np.array([13,26,29,34,50,53,91,94,103,112,117,132,138,141,155,174,197,198])
 
 def choose_starting_nodes():
     return np.random.choice(starting_nodes, size=6, replace=False)
+
+surface_points = np.array([3,8,13,18,24]) # Mr. X's location is revealed after turns 3,8,13,18,24
