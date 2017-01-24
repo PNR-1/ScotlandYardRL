0299f317f284372e1dd4acf90f106a5d16ffecc1
Sahith Dambekodi <SND96@users.noreply.github.com>
2017-01-24 16:45:37 +0530

    Made weights and biases size less random

--- rl_backend/modelX.py
+++ rl_backend/modelX.py
@@ -9,40 +9,39 @@ class Model(object):
     def __init__(self):
         tf.reset_default_graph
         self.learning_rate = 0.001
-
         self.columns = 1415
         self.X = tf.placeholder(shape=[None, self.columns], dtype=tf.float32, name="X")
     # The TD target value
-        self.Y = tf.placeholder(shape=[None,1], dtype=tf.float32, name="Y")
+        self.Y = tf.placeholder(shape=[1, None], dtype=tf.float32, name="Y")
+
+        self.hidden1 = int(self.columns/2)
+        self.hidden2 = int(self.columns/2)
+        self.out = 1
+
+        self.weights = {
+            'w1' : tf.Variable(tf.random_normal([self.columns, self.hidden1])),
+            'w2' : tf.Variable(tf.random_normal([self.hidden1, self.hidden2])),
+            'out' : tf.Variable(tf.random_normal([self.hidden2, self.out]))
+        }
+
+        self.biases = {
+            'b1': tf.Variable(tf.random_normal([self.hidden1])),
+            'b2': tf.Variable(tf.random_normal([self.hidden2])),
+            'out': tf.Variable(tf.random_normal([self.out]))
+            }
 
 
-        self.W1 = tf.Variable(tf.random_normal([self.columns, 128]))
-        self.b1 = tf.Variable(tf.random_normal([128]))
-        self.hidden1 = tf.nn.relu(tf.matmul(self.X, self.W1) + self.b1)
-
-        self.W2 = tf.Variable(tf.random_normal([128, 64]))
-        self.b2 = tf.Variable(tf.random_normal([64]))
-        self.hidden2 = tf.nn.relu(tf.matmul(self.hidden1, self.W2) + self.b2)
-
-        self.W3 = tf.Variable(tf.random_normal([64, 1]))
-        self.b3 = tf.Variable(tf.random_normal([1]))
-        self.pred = tf.nn.relu(tf.matmul(self.hidden2, self.W3) + self.b3)
+        self.layer1 = tf.nn.relu(tf.matmul(self.X, self.weights['w1']) + self.biases['b1'])
+        self.layer2 = tf.nn.relu(tf.matmul(self.layer1, self.weights['w2']) + self.biases['b2'])
+        self.pred = tf.nn.relu(tf.matmul(self.layer2, self.weights['out']) + self.biases['out'])
 
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.pred, labels=self.Y))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
-        #print("hello")
-        self.init = tf.initialize_all_variables()
+        self.init = tf.global_variables_initializer()
+
         global sess
         sess = tf.Session()
         sess.run(self.init)
-
-
-
-    def initialize(self):
-        init = tf.global_variables_initializer()
-        with tf.Session() as sess:
-            sess.run(init)
-        return sess
 
 
     def predict(self,x):
@@ -51,5 +50,4 @@ class Model(object):
 
 
     def optimize(self,x, y):
-        #sess = tf.Session()
         sess.run([self.optimizer, self.cost], feed_dict={self.X: x, self.Y: y})
