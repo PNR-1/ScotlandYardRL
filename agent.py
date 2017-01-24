04549c68f4b8a574f6d0a0a6aed6543fc8d97134
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2017-01-24 19:23:16 +0530

    Update agent.py
    you hadn't updated each ones observation individually. We need to rebuild this base a little bit better.

--- agent.py
+++ agent.py
@@ -10,11 +10,11 @@ import game
 class RunAgent(object):
     def __init__(self):
         self.mdx = mdX.Model()
-        self.mdd = [None] * 6
-        for i in range (6):
+        self.mdd = [None] * 5
+        for i in range (5):
             self.mdd[i] = mdD.Model()
         self.q_target = []
-        self.d_last_obs = []
+        self.d_last_obs = [[0] * 1427] * 5
         self.x_last_obs = []
 
         self.log_path = './log'
@@ -42,7 +42,7 @@ class RunAgent(object):
 
 
         for i in range(self.epsilon_decay_steps):
-            print('\nDetectives: ',self.detective_win,'\t\tMR X: ',self.x_win)
+            print('Detectives: ',self.detective_win,'\t\tMR X: ',self.x_win)
             print('Running Episode: ',i,end = '\t')
             self.explore = i
             print('Explore = ',self.epsilon[self.explore],end = '\t')
@@ -65,8 +65,6 @@ class RunAgent(object):
         self.SL = game.ScotlandYard()
         self.SL.initialize_game()
         done = False
-        import time
-        import os
         while done == False:
             present_observation,sub_turn = self.SL.observe()
             actions = self.SL.valid_moves()
@@ -74,7 +72,7 @@ class RunAgent(object):
             if sub_turn == 0:
                 optimum_action,_ = self.getOptimum_Action(present_observation,actions,self.mdx)
             else:
-                optimum_action,_ = self.getOptimum_Action(present_observation,actions,self.mdd[sub_turn])
+                optimum_action,_ = self.getOptimum_Action(present_observation,actions,self.mdd[sub_turn - 1])
 
                 #Have optimum_action
 
@@ -97,15 +95,15 @@ class RunAgent(object):
                 self.mdx.optimize([state_used], [q_target])
 
             else:
-                _,Q_max = self.getOptimum_Action(next_observation,actions,self.mdd[sub_turn])
+                _,Q_max = self.getOptimum_Action(next_observation,actions,self.mdd[sub_turn - 1])
                 q_target = [Q_max + reward]
                     #print ('Q_traget',q_target)
-                self.mdd[sub_turn].optimize([state_used], [q_target])
+                self.mdd[sub_turn -1].optimize([state_used], [q_target])
             if done == False:
                 if sub_turn == 0:
                     self.x_last_obs = state_used
                 elif sub_turn == 5:
-                    self.d_last_obs = state_used
+                    self.d_last_obs[sub_turn - 1] = state_used
 
         self.reward = reward
         print('Reward: ',reward)
@@ -113,9 +111,10 @@ class RunAgent(object):
             self.detective_win = self.detective_win + 1
         else:
             self.x_win = self.x_win + 1
+
         if sub_turn == 0:
             for i in range(5):
-                self.mdd[i].optimize([self.d_last_obs],[[Q_max]])
+                self.mdd[i].optimize([self.d_last_obs[i]],[[Q_max]])
         else:
             self.mdx.optimize([self.x_last_obs],[[Q_max]])
 
