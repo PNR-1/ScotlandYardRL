883b5601d2b171b02222c70ea9bea9fd01c7d7e6
Sahith Dambekodi <SND96@users.noreply.github.com>
2017-01-24 16:46:02 +0530

    Made weights and biases sizes less random

--- rl_backend/modelDetective.py
+++ rl_backend/modelDetective.py
@@ -9,7 +9,7 @@ class Model(object):
     def __init__(self):
         tf.reset_default_graph
         self.learning_rate = 0.001
-        self.columns = 1415
+        self.columns = 1432
         self.X = tf.placeholder(shape=[None, self.columns], dtype=tf.float32, name="X")
     # The TD target value
         self.Y = tf.placeholder(shape=[1, None], dtype=tf.float32, name="Y")
