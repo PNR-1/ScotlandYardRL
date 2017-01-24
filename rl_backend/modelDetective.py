83959654071bedd24ea0373348b070792d5750be
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2017-01-24 19:25:44 +0530

    Update modelDetective.py

--- rl_backend/modelDetective.py
+++ rl_backend/modelDetective.py
@@ -1,15 +1,12 @@
-import itertools
 import numpy as np
-import os
-import random
 import tensorflow as tf
-import utilities
+
 class Model(object):
 
     def __init__(self):
         tf.reset_default_graph
         self.learning_rate = 0.001
-        self.columns = 1432
+        self.columns = 1427
         self.X = tf.placeholder(shape=[None, self.columns], dtype=tf.float32, name="X")
     # The TD target value
         self.Y = tf.placeholder(shape=[1, None], dtype=tf.float32, name="Y")
@@ -37,7 +34,7 @@ class Model(object):
 
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.pred, labels=self.Y))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
-        self.init = tf.global_variables_initializer()
+        self.init = tf.initialize_all_variables()
 
         global sess
         sess = tf.Session()
