dc79a104bd0c3da889fb09bea9ca5aa0f3b9ed19
Sahith Dambekodi <sahithdn@gmail.com>
2017-02-01 22:11:18 +0530

    New model

--- rl_backend/modelDetective.py
+++ rl_backend/modelDetective.py
@@ -1,48 +1,55 @@
-import itertools
 import numpy as np
-import os
-import random
 import tensorflow as tf
-import utilities
 class Model(object):
 
     def __init__(self):
         tf.reset_default_graph
         self.learning_rate = 0.001
-
         self.columns = 1427
         self.X = tf.placeholder(shape=[None, self.columns], dtype=tf.float32, name="X")
     # The TD target value
-        self.Y = tf.placeholder(shape=[None,1], dtype=tf.float32, name="Y")
+        self.Y = tf.placeholder(shape=[1, None], dtype=tf.float32, name="Y")
+
+        self.hidden1 = int(self.columns/2)
+        self.hidden2 = int(self.columns/2)
+        self.hidden3 = int(self.columns/4)
+        self.hidden4 = int(self.columns/4)
+        self.out = 1
+
+        self.weights = {
+            'w1' : tf.Variable(tf.random_normal([self.columns, self.hidden1])),
+            'w2' : tf.Variable(tf.random_normal([self.hidden1, self.hidden2])),
+            'w3' : tf.Variable(tf.random_normal([self.hidden2, self.hidden3])),
+            'w4' : tf.Variable(tf.random_normal([self.hidden3, self.hidden4])),
+            'out' : tf.Variable(tf.random_normal([self.hidden4, self.out]))
+        }
+
+        self.biases = {
+            'b1': tf.Variable(tf.random_normal([self.hidden1])),
+            'b2': tf.Variable(tf.random_normal([self.hidden2])),
+            'b3': tf.Variable(tf.random_normal([self.hidden3])),
+            'b4': tf.Variable(tf.random_normal([self.hidden4])),
+            'out': tf.Variable(tf.random_normal([self.out]))
+            }
 
 
-        self.W1 = tf.Variable(tf.random_normal([self.columns, 128]))
-        self.b1 = tf.Variable(tf.random_normal([128]))
-        self.hidden1 = tf.nn.relu(tf.matmul(self.X, self.W1) + self.b1)
+        self.layer1 = tf.nn.relu(tf.matmul(self.X, self.weights['w1']) + self.biases['b1'])
+        self.layer2 = tf.nn.relu(tf.matmul(self.layer1, self.weights['w2']) + self.biases['b2'])
+        self.layer3 = tf.nn.relu(tf.matmul(self.layer2, self.weights['w3']) + self.biases['b3'])
+        self.layer4 = tf.nn.relu(tf.matmul(self.layer3, self.weights['w4']) + self.biases['b4'])
 
-        self.W2 = tf.Variable(tf.random_normal([128, 64]))
-        self.b2 = tf.Variable(tf.random_normal([64]))
-        self.hidden2 = tf.nn.relu(tf.matmul(self.hidden1, self.W2) + self.b2)
-
-        self.W3 = tf.Variable(tf.random_normal([64, 1]))
-        self.b3 = tf.Variable(tf.random_normal([1]))
-        self.pred = tf.nn.relu(tf.matmul(self.hidden2, self.W3) + self.b3)
+        self.pred = tf.nn.relu(tf.matmul(self.layer4, self.weights['out']) + self.biases['out'])
 
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.pred, labels=self.Y))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
-        #print("hello")
-        self.init = tf.global_variables_initializer()
+        self.init = tf.global_variables_initializer() # Do not change this. The line you used is deprecated.
+        self.save_path = None
+        # Creating a saver object with a maximum of 5 latest models to be saved. The oldest one is automatically deleted.
+        self.saver = tf.train.Saver(max_to_keep = 5)
+
         global sess
         sess = tf.Session()
         sess.run(self.init)
-
-
-
-    def initialize(self):
-        init = tf.global_variables_initializer()
-        with tf.Session() as sess:
-            sess.run(init)
-        return sess
 
 
     def predict(self,x):
@@ -51,5 +58,13 @@ class Model(object):
 
 
     def optimize(self,x, y):
-        #sess = tf.Session()
         sess.run([self.optimizer, self.cost], feed_dict={self.X: x, self.Y: y})
+
+    def save(self, episode):
+        # Saving the file path and storing the file path. The episode which it saves is appended to the file name.
+        # Will make this more organized.
+        self.save_path = self.saver.save(sess, 'models/detective_models/', global_step = episode)
+
+    def restore(self):
+        # Restoring the file from a previous save path.
+        self.saver.restore(sess, self.save_path)
